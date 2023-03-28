981a916174d8de1c7666c924511577b95860d3f3
Dan <98132259+Dan090807@users.noreply.github.com>
2023-03-28 07:55:02 -0600

    Update VectorHeap.java

--- VectorHeap.java
+++ VectorHeap.java
@@ -1,6 +1,14 @@
 import java.util.PriorityQueue;
 
-public class VectorHeap {
+public class VectorHeap<Integer extends Comparable<Integer>> {
 
     PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+    public void add(Integer integer) {
+        priorityQueue.add(integer);
+    }
+
+    public void remove(Integer integer) {
+        priorityQueue.remove(integer);
+    }
 }
